49748ac8ed78147a55381c6f2327efa4ae191468
Kal <kal.aalrajhi@gmail.com>
2023-07-09 17:39:56 -0400

    Add padding to description text

--- Apple-Frameworks/DetailView.swift
+++ Apple-Frameworks/DetailView.swift
@@ -10,22 +10,10 @@ import SwiftUI
 struct DetailView: View {
     var body: some View {
         VStack {
+            Spacer()
             FrameworkTitleView(framework: MockData.frameworks[0])
-            VStack {
-                Text(MockData.frameworks[0].description)
-                Spacer()
-                Button {
-                    // Do something
-                } label: {
-                    Text("Learn More")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .frame(width: 300, height: 60)
-                        .background(.red)
-                        .cornerRadius(10)
-                }
-            }
-            .padding()
+            FrameworkDetailView(description: MockData.frameworks[0].description,
+                                frameworkURL: MockData.frameworks[0].urlString)
         }
     }
 }
@@ -34,5 +22,28 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView()
             .preferredColorScheme(.dark)
+    }
+}
+
+struct FrameworkDetailView: View {
+    let description: String
+    let frameworkURL: String
+    
+    var body: some View {
+        VStack {
+            Text(description)
+                .padding()
+            Spacer()
+            Button {
+//                Link("", destination: URL(string: frameworkURL)!)
+            } label: {
+                Text("Learn More")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 60)
+                    .background(.red)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
