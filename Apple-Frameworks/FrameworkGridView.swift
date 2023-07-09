0ef5cddc0dd2e60fb7398459fedda9f7e52567cb
Kal <kal.aalrajhi@gmail.com>
2023-07-09 11:34:02 -0400

    Create framework title struct to dynamically create icons

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -10,15 +10,7 @@ import SwiftUI
 struct FrameworkGridView: View {
     var body: some View {
         VStack {
-            Image("arkit")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 90, height: 90)
-            Text("arkit")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit() // shrink if needed
-                .minimumScaleFactor(0.6) // but only shrink by 60%
+            FrameworkTitleView(name: "arkit", imageName: "arkit")
         }
     }
 }
@@ -26,5 +18,24 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(name)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90, height: 90)
+            Text(imageName)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit() // shrink if needed
+                .minimumScaleFactor(0.6) // but only shrink by 60%
+        }
     }
 }
