054fb1e3c11778e970bd166935710d8f304e593f
Kal <kal.aalrajhi@gmail.com>
2023-07-09 11:10:16 -0400

    Add framework and respective data

---
+++ Apple-Frameworks/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by Dr Cpt Blackbeard on 7/9/23.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
