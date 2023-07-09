5e374447977a20e7125e49ffbf4a1ee92821a0b0
Kal <kal.aalrajhi@gmail.com>
2023-07-09 11:02:32 -0400

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Dr Cpt Blackbeard on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
